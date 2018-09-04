8e9671bae14e4bd29a8befc1564d6e2fbb902377
chloe.spilker <chloe.spilker@CLC-LAB209-04.TCSD.tooelesd.org>
2018-09-04 13:19:39 -0600

    first commit

---
+++ src/c/spik3_hello/world2/Hello_World_2.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package c.spik3_hello.world2;
+
+/**
+ * @author chloe.spilker
+ * hello world 2
+ */
+public class Hello_World_2 {
+
+	/**
+	 * constructor: useless for now?
+	 */
+	public Hello_World_2() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World 2.0");
+
+	}
+
+}
